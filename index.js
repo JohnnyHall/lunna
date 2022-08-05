773776858fdac498ccf96e0e3a425058fe95d6de
João Tápparo <joaovtapparo@gmail.com>
2022-08-04 22:45:21 -0300

    change index

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const dotenv = require('dotenv');
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-dotenv.config();
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-console.log(process.env.A);
-console.log(process.env.B);
-console.log(process.env.DISCORD_TOKEN);
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
